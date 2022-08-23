85acd147cda416eca7aa566cef7f81617071ac90
Vlad Ageysha <ander123fakk@gmail.com>
2022-08-23 14:24:46 +0300

    Made transition for slider

---
+++ scripts/slider.js
@@ -0,0 +1,14 @@
+const radioBtn = document.querySelectorAll('.slider__radio-btn');
+const slideContainer = document.querySelector('.slider__slide-container');
+
+for (let i = 0; i < radioBtn.length; i++) {
+    radioBtn[i].addEventListener('click', () => {
+        const radioBtnActive = document.querySelector('.slider__radio-btn_active');
+
+        radioBtnActive.classList.remove('slider__radio-btn_active');
+        radioBtn[i].classList.add('slider__radio-btn_active');
+
+        if (i === 0) slideContainer.style.left = '0';
+        else slideContainer.style.left = 'calc(-100%)';
+    })
+}
